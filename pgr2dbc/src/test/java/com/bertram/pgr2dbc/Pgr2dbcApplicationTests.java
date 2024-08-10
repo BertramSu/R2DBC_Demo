f8de1e77e64fef08358c864c3f3fcd54a11e0f3f
BertramSu <40878105+BertramSu@users.noreply.github.com>
2024-08-09 23:29:26 -0500

    Boot initalizer

---
+++ pgr2dbc/src/test/java/com/bertram/pgr2dbc/Pgr2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertram.pgr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pgr2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
